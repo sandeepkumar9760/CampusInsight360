a33a006e5b5a6c714f71e850d7fd7102ee0f4bfa
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-24 10:55:19 +0530

    Update views.py

--- campus360/backend_360/views.py
+++ campus360/backend_360/views.py
@@ -10,3 +10,23 @@ from .models import CampusBlock, Classroom, Course, Faculty, Student
 # -------------------------------------------------
 def login_view(request):
     return render(request, "login.html")
+
+# --------------------------------------------------
+# DASHBOARD VIEW
+# --------------------------------------------------
+def dashboard(request):
+    total_blocks = CampusBlock.objects.count()
+    total_classrooms = Classroom.objects.count()
+    total_courses = Course.objects.count()
+    total_faculty = Faculty.objects.count()
+    total_students = Student.objects.count()
+
+    context = {
+        "total_blocks": total_blocks,
+        "total_classrooms": total_classrooms,
+        "total_courses": total_courses,
+        "total_faculty": total_faculty,
+        "total_students": total_students,
+    }
+
+    return render(request, "index.html", context)
